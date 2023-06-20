0b2cb292070b8718e3b8489b8d326a90b3c45e83
SteCard <stefanocardillo14@gmail.com>
2023-06-20 03:28:27 +0200

    completo

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,18 @@
+<!-- SCRIPT JS -->
+<script>
+  export default {
+    
+  }
+</script>
+
+<!-- TEMPLATE HTML -->
+<template>
+  <div>
+    <h1>La mia prima app con Vite!</h1>
+  </div>
+</template>
+
+<!-- STYLE CSS -->
+<style scoped>
+  
+</style>

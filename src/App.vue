de1038252b16fe464cd5837248de569681b9e56e
SteCard <stefanocardillo14@gmail.com>
2023-06-20 10:00:47 +0200

    BONUS - AppJumbotron

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,12 @@
 
 import AppTitle from './components/AppTitle.vue'; //Comunico l'esistenza della componente AppTitle
 
+import AppJumbotron from './components/AppJumbotron.vue'; //Comunico l'esistenza della componente AppTitle
+
 export default {
   components:{
-    AppTitle //Comunico che può utilizzare la componente AppTitle
+    AppTitle, //Comunico che può utilizzare la componente AppTitle
+    AppJumbotron
   }
 }
 </script>
@@ -12,6 +15,7 @@ export default {
 <template>
   <div>
     <AppTitle /> <!-- Utilizzo la componente -->
+    <AppJumbotron />
   </div>
 </template>
 
---
+++ src/components/AppJumbotron.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <div>
+        <img src="https://picsum.photos/200/300">
+    </div>
+</template>
+
+<style>
+    
+</style>
